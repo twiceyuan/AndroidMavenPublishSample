60798f809473bdbf4f18b65d0213f90e21b91485
twiceYuan <twiceyuan@gmail.com>
2020-07-16 14:20:02 +0800

    添加一个字符串资源合并的演示；升级依赖版本配置

--- app/src/main/java/com/twiceyuan/androidmavenpublishsample/MainActivity.kt
+++ app/src/main/java/com/twiceyuan/androidmavenpublishsample/MainActivity.kt
@@ -3,12 +3,15 @@ package com.twiceyuan.androidmavenpublishsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.twiceyuan.mavenpublish.samplelib.TestLib
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_resource.setText(R.string.test_string)
 
         TestLib().test()
     }
